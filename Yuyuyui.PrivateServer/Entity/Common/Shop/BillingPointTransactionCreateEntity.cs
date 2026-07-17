5365f3bd88713f251e7614abe6b2f4c3abf63b50
burning calamity <200230264+burning-calamity@users.noreply.github.com>
2026-07-17 14:52:14 +0200

    Use base card keys and apply billing shop effects

---
+++ Yuyuyui.PrivateServer/Entity/Common/Shop/BillingPointTransactionCreateEntity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Yuyuyui.PrivateServer
+{
+    public class BillingPointTransactionCreateEntity : BaseEntity<BillingPointTransactionCreateEntity>
+    {
+        public BillingPointTransactionCreateEntity(Uri requestUri, string httpMethod, Dictionary<string, string> requestHeaders, byte[] requestBody, RouteConfig config)
+            : base(requestUri, httpMethod, requestHeaders, requestBody, config) { }
+
+        protected override Task ProcessRequest()
+        {
+            responseBody = Serialize(new Response { transaction = new Transaction { id = long.Parse(Utils.GenerateRandomDigit(8)) } });
+            SetBasicResponseHeaders();
+            return Task.CompletedTask;
+        }
+
+        public class Response
+        {
+            public Transaction transaction { get; set; } = new();
+        }
+
+        public class Transaction
+        {
+            public long id { get; set; }
+        }
+    }
+}
