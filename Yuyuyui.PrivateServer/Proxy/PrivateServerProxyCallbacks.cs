05d80f461e05758e25e9626f11a3d897c00f3159
burning calamity <200230264+burning-calamity@users.noreply.github.com>
2026-07-17 13:33:17 +0200

    Keep fallback gacha rolls on selected gacha

--- Yuyuyui.PrivateServer/Proxy/PrivateServerProxyCallbacks.cs
+++ Yuyuyui.PrivateServer/Proxy/PrivateServerProxyCallbacks.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -45,6 +46,18 @@ namespace Yuyuyui.PrivateServer
                     headersAndBody.Item1,
                     headersAndBody.Item2,
                     $"{apiError.body}");
+                await entity.Process();
+            }
+            catch (Exception exception)
+            {
+                Utils.LogError(exception.ToString());
+
+                entity = new NoopEntity(
+                    e.HttpClient.Request.RequestUri,
+                    e.HttpClient.Request.Method,
+                    new Dictionary<string, string>(),
+                    Array.Empty<byte>(),
+                    new RouteConfig(entity.RequestUri.AbsolutePath, e.HttpClient.Request.Method));
                 await entity.Process();
             }
 
