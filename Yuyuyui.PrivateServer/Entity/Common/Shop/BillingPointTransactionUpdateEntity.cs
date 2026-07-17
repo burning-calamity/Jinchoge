e50ecf902de933a6dbc52d1005f3e7fae0cee36a
burning calamity <200230264+burning-calamity@users.noreply.github.com>
2026-07-17 13:18:44 +0200

    Add API fallback responses

---
+++ Yuyuyui.PrivateServer/Entity/Common/Shop/BillingPointTransactionUpdateEntity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Yuyuyui.PrivateServer
+{
+    public class BillingPointTransactionUpdateEntity : BaseEntity<BillingPointTransactionUpdateEntity>
+    {
+        public BillingPointTransactionUpdateEntity(Uri requestUri, string httpMethod, Dictionary<string, string> requestHeaders, byte[] requestBody, RouteConfig config)
+            : base(requestUri, httpMethod, requestHeaders, requestBody, config) { }
+
+        protected override Task ProcessRequest()
+        {
+            long transactionId = long.Parse(GetPathParameter("transaction_id"));
+            responseBody = Serialize(new Response { transaction = new Transaction { id = transactionId }, completed = true });
+            SetBasicResponseHeaders();
+            return Task.CompletedTask;
+        }
+
+        public class Response
+        {
+            public Transaction transaction { get; set; } = new();
+            public bool completed { get; set; }
+        }
+
+        public class Transaction
+        {
+            public long id { get; set; }
+        }
+    }
+}
---
+++ Yuyuyui.PrivateServer/Entity/Common/Shop/ShopProductTransactionUpdateEntity.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Yuyuyui.PrivateServer
+{
+    public class ShopProductTransactionUpdateEntity : BaseEntity<ShopProductTransactionUpdateEntity>
+    {
+        public ShopProductTransactionUpdateEntity(
+            Uri requestUri,
+            string httpMethod,
+            Dictionary<string, string> requestHeaders,
+            byte[] requestBody,
+            RouteConfig config)
+            : base(requestUri, httpMethod, requestHeaders, requestBody, config)
+        {
+        }
+
+        protected override Task ProcessRequest()
+        {
+            long productId = long.Parse(GetPathParameter("product_id"));
+            long transactionId = long.Parse(GetPathParameter("transaction_id"));
+
+            Response responseObj = new()
+            {
+                transaction = new()
+                {
+                    id = transactionId
+                },
+                product = new()
+                {
+                    id = productId,
+                    purchased_quantity = 1
+                }
+            };
+
+            responseBody = Serialize(responseObj);
+            SetBasicResponseHeaders();
+
+            return Task.CompletedTask;
+        }
+
+        public class Response
+        {
+            public Transaction transaction { get; set; } = new();
+            public Product product { get; set; } = new();
+
+            public class Transaction
+            {
+                public long id { get; set; }
+            }
+
+            public class Product
+            {
+                public long id { get; set; }
+                public int purchased_quantity { get; set; }
+            }
+        }
+    }
+}
