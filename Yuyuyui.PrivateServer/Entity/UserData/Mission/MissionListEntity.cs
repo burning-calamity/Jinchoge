8c698f934dc5c3153b92adfa3ffb501149fddc72
burning calamity <200230264+burning-calamity@users.noreply.github.com>
2026-07-17 17:22:06 +0200

    Persist billing point transactions

---
+++ Yuyuyui.PrivateServer/Entity/UserData/Mission/MissionListEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Yuyuyui.PrivateServer
+{
+    public class MissionListEntity : BaseEntity<MissionListEntity>
+    {
+        public MissionListEntity(Uri requestUri, string httpMethod, Dictionary<string, string> requestHeaders, byte[] requestBody, RouteConfig config)
+            : base(requestUri, httpMethod, requestHeaders, requestBody, config) { }
+
+        protected override Task ProcessRequest()
+        {
+            responseBody = Serialize(new Response());
+            SetBasicResponseHeaders();
+            return Task.CompletedTask;
+        }
+
+        public class Response
+        {
+            public IList<object> missions { get; set; } = new List<object>();
+            public IList<object> mission_adventures { get; set; } = new List<object>();
+        }
+    }
+}
