ec6410d2b7f5e21a1de9783d1fa31abbd09415b4
burning calamity <200230264+burning-calamity@users.noreply.github.com>
2026-07-17 13:02:22 +0200

    Allow multiple routes per entity type

---
+++ Yuyuyui.PrivateServer/Entity/Common/Shop/BirthdateRegistrationEntity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Yuyuyui.PrivateServer
+{
+    public class BirthdateRegistrationEntity : BaseEntity<BirthdateRegistrationEntity>
+    {
+        public BirthdateRegistrationEntity(
+            Uri requestUri,
+            string httpMethod,
+            Dictionary<string, string> requestHeaders,
+            byte[] requestBody,
+            RouteConfig config)
+            : base(requestUri, httpMethod, requestHeaders, requestBody, config)
+        {
+        }
+
+        protected override Task ProcessRequest()
+        {
+            responseBody = Serialize(new Response());
+            SetBasicResponseHeaders();
+            return Task.CompletedTask;
+        }
+
+        public class Response
+        {
+            public bool registered { get; set; } = true;
+        }
+    }
+}
